d772b6cc1972bafe0673ce4664c6385a5e78d0c0
Ralph Sliger <ralphsliger1@gmail.com>
2021-12-10 17:33:39 -0500

    correcion controller

--- src/main/java/co/com/sofka/calendar/controllers/SchedulerController.java
+++ src/main/java/co/com/sofka/calendar/controllers/SchedulerController.java
@@ -4,27 +4,30 @@ package co.com.sofka.calendar.controllers;
 import co.com.sofka.calendar.model.ProgramDate;
 import co.com.sofka.calendar.services.SchedulerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 @RestController
-@RequestMapping("api/v1/scheduler")
+@CrossOrigin("*")
+@RequestMapping("/scheduler")
 public class SchedulerController {
 
     @Autowired
     SchedulerService service;
 
-    @GetMapping("/generateSchedule/{id}/{date}")
-    private Flux<ProgramDate> generateSchedule(@PathVariable String id, @PathVariable String iDate ) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        LocalDate local = LocalDate.parse(iDate, formatter);
+    @GetMapping("/generate/{date}/{id}")
+    private Flux<ProgramDate> generateSchedule(@PathVariable String date, @PathVariable String id ) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        LocalDate local = LocalDate.parse(date, formatter);
         return service.generateCalendar(id, local);
+    }
+
+    @GetMapping("/test")
+    private Flux<ProgramDate> test() {
+        return service.generateCalendar("61b3d0e477be7ce9d742674e", LocalDate.of(2021,11,12));
     }
 
 }
